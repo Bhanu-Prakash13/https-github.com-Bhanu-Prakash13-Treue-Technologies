398b2f6bf3f7629b1f6d811d6822352baba3f5ec
Bhanu-Prakash13 <147971184+Bhanu-Prakash13@users.noreply.github.com>
2023-10-15 21:25:07 +0530

    Image Gallery

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+const thumbnails = document.querySelectorAll('.thumbnail');
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightbox-image');
+const lightboxText = document.getElementById('lightbox-text');
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) => {
-        if (e.target.innerHTML == '=') {
-            string = eval(string);
-            input.value = string;
-        } else if (e.target.innerHTML == 'AC') {
-            string = "";
-            input.value = string;
-        } else if (e.target.innerHTML == 'DEL') {
-            string = string.substring(0, string.length - 1);
-            input.value = string;
-        } else {
-            string += e.target.innerHTML;
-            input.value = string;
-        }
+// Array of image URLs and text descriptions
+const imageData = [
+  {
+    src: 'Images/img1.jpg',
+    text: ' Image 1',
+  },
+  {
+    src: 'Images/img2.jpeg',
+    text: ' Image 2',
+  },
+  {
+    src: 'Images/img3.jpg',
+    text: ' Image 3',
+  },
+  {
+    src: 'Images/img4.jpg',
+    text: ' Image 4',
+  },
+  {
+    src: 'Images/img5.jpg',
+    text: ' Image 5',
+  },
+  {
+    src: 'Images/img6.jpg',
+    text: ' Image 6',
+    text: ' Image 6',
+  },
+  {
+    src: 'Images/img7.jpg',
+    text: ' Image 7',
+  },
+  {
+    src: 'Images/img8.jpg',
+    text: ' Image 8',
+  },
+  {
+    src: 'Images/img9.jpg',
+    text: ' Image 9',
+  },
+  {
+    src: 'Images/img10.jpg',
+    text: ' Image 10',
+  },
+  // Add more image URLs and text descriptions here as needed
+];
 
-    })
-})
+function openLightbox(index) {
+  const imageInfo = imageData[index];
+  lightboxImage.src = imageInfo.src;
+  lightboxText.textContent = imageInfo.text;
+  lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+  lightbox.style.display = 'none';
+}
+
+// Close the lightbox when pressing the Escape key
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeLightbox();
+  }
+});
